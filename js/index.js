79e0657e8048fa153ca641a16682a372074b244d
Andrew Jones <Andrew.jones13@snhu.edu>
2022-08-05 13:25:25 -0400

    Update navbar and make it responsive

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-})
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
